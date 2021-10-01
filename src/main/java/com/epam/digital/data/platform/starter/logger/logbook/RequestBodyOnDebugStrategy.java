1cf1a1cd3c05473c945a067a4a391278fb5c7247
Eduard_Kuznetsov <eduard_kuznetsov@epam.com>
2021-10-01 11:04:07 +0300

    [EDP-DDM-9487] added responseCode to logs
    Change-Id: I2eb7afc5f33365750e69d1136a8d7db2068dd1e0

--- src/main/java/com/epam/digital/data/platform/starter/logger/logbook/RequestBodyOnDebugStrategy.java
+++ src/main/java/com/epam/digital/data/platform/starter/logger/logbook/RequestBodyOnDebugStrategy.java
@@ -2,9 +2,12 @@ package com.epam.digital.data.platform.starter.logger.logbook;
 
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.lang.NonNull;
+import org.zalando.logbook.Correlation;
 import org.zalando.logbook.HttpRequest;
 import org.zalando.logbook.HttpResponse;
+import org.zalando.logbook.Sink;
 import org.zalando.logbook.Strategy;
 
 /**
@@ -12,6 +15,8 @@ import org.zalando.logbook.Strategy;
  */
 @Slf4j
 public class RequestBodyOnDebugStrategy implements Strategy {
+
+  private static final String RESPONSE_CODE_MDC_KEY = "responseCode";
 
   @Override
   public HttpRequest process(@NonNull HttpRequest request) throws IOException {
@@ -23,5 +28,20 @@ public class RequestBodyOnDebugStrategy implements Strategy {
       @NonNull HttpRequest request,
       @NonNull HttpResponse response) throws IOException {
     return log.isDebugEnabled() ? response.withBody() : response.withoutBody();
+  }
+
+  @Override
+  public void write(
+      @NonNull Correlation correlation,
+      @NonNull HttpRequest request,
+      @NonNull HttpResponse response,
+      @NonNull Sink sink)
+      throws IOException {
+    try {
+      MDC.put(RESPONSE_CODE_MDC_KEY, String.valueOf(response.getStatus()));
+      sink.write(correlation, request, response);
+    } finally {
+      MDC.remove(RESPONSE_CODE_MDC_KEY);
+    }
   }
 }
